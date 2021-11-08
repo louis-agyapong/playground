6a0e83c9f8a393531239c144c3566060eb85a47f
Louis Agyapong <agyapong.louis@gmail.com>
2021-11-08 13:38:22 +0000

    type hints

--- class_two.py
+++ class_two.py
@@ -1,7 +1,7 @@
 class Car:
     """A simple attempt to represent a car."""
 
-    def __init__(self, make, model, year, max_speed):
+    def __init__(self, make: str, model: str, year: int, max_speed: int) -> None:
         """Initialize attributes to describe a car."""
         self.make = make
         self.model = model
@@ -11,20 +11,23 @@ class Car:
         self.fuel_capacity = 100
 
     def __str__(self) -> str:
-        return f"{self.make} {self.model} {self.year}"
+        return f"{self.make} {self.model} {str(self.year)}"
 
-    def drive_car(self):
+    def drive_car(self) -> str:
         """Control the cars movement."""
-        print("The car is now moving")
+        return "The car is now moving"
 
-    def add_fuel(self, amount):
+    def calculate_fuel(self, distance: int) -> str:
+        """Calculate the current fuel based on distance travelled."""
+        return f"Current fuel: {str(self.fuel_level)}"
+
+    def add_fuel(self, amount) -> str:
         """add fuel"""
         fuel_level = self.fuel_level + amount
 
         if fuel_level > self.fuel_capacity:
-            print("Fuel capacity exceeded.")
-        elif fuel_level <= self.fuel_capacity:
-            print("Fuel added.")
+            return "Fuel capacity exceeded."
+        return "Fuel added."
 
 
-car = Car("Honda", "Civic", "2018", 100)
+car = Car("Honda", "Civic", 2018, 100)
