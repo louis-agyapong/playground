b49f70adf4c068672ff5650ef165d56bbd7e1060
Louis Agyapong <agyapong.louis@gmail.com>
2022-01-30 22:40:55 +0000

    instance method

--- classses.py
+++ classses.py
@@ -11,8 +11,13 @@ class Dog:
     def description(self) -> str:
         return f"{self.name} is {self.age} years old"
 
+    # instance method
+    def speak(self, sound: str) -> str:
+        return f"{self.name} says {sound}"
+
 
 buddy = Dog("Buddy", 6)
 miles = Dog("Miles", 4)
 
 print(buddy.description())
+print(buddy.speak("Woof!"))
