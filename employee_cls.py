85efe7fb5c7a2ddbc4d14eba7e01894e6facdd67
Louis Agyapong <agyapong.louis@gmail.com>
2022-02-01 17:37:37 +0000

    class variables

--- employee_cls.py
+++ employee_cls.py
@@ -1,4 +1,7 @@
 class Employee:
+
+    raise_amount = 1.04
+
     def __init__(self, first: str, last: str, pay: int) -> None:
         self.first = first
         self.last = last
@@ -8,12 +11,13 @@ class Employee:
     def fullname(self) -> str:
         return f"{self.first.title()} {self.last.title()}"
 
+    def apply_raise(self) -> None:
+        self.pay = int(self.pay * Employee.raise_amount)
+
 
 emp_1 = Employee("louis", "agyapong", 50000)
 emp_2 = Employee("nana", "kyei", 60000)
 
-print(emp_1)
-print(emp_2)
-
-print(emp_1.fullname())
-print(emp_2.fullname())
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
