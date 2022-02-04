2c576d54e93fadca444f0849e4ee30a47239a397
Louis Agyapong <agyapong.louis@gmail.com>
2022-02-04 10:26:43 +0000

    days until birthday

--- dates.py
+++ dates.py
@@ -5,5 +5,14 @@ tdelta = datetime.timedelta(days=7)
 one_week_ago = today - tdelta
 next_week = today + tdelta
 
+"""
+How many days until my birthday
+"""
+birth_day = datetime.date(1990, 10, 18)
+
+days_until_birthday = today - birth_day
+
+print(days_until_birthday)
+
 print(one_week_ago)
 print(next_week)
