250ec8faf80d5b9f55c107669746d3c687f96ae3
Louis Agyapong <agyapong.louis@gmail.com>
2022-02-07 21:27:14 +0000

    inventory class

--- dataclasses.py
+++ dataclasses.py
@@ -1,18 +1,18 @@
-from dataclasses import dataclass
 
-
-@dataclass
 class InventoryItem:
     """
     Class for keeping track of an item in inventory.
+    will be refactored to use dataclasses.
     """
-    name: str
-    unit_price: float
-    quantity_on_hand: int = 0
+    def __init__(self, name: str, unit_price: float, quantity_on_hand: int = 0) -> None:
+        self.name = name
+        self.unit_price = unit_price
+        self.quantity_on_hand = quantity_on_hand
 
     def total_cost(self) -> float:
         return self.unit_price * self.quantity_on_hand
 
-item1 = InventoryItem("Widget", 10, 5)
-print(item1.total_cost())
+if __name__ == "__main__":
+    item1 = InventoryItem("Widget", 10, 5)
+    print(item1.name)
 
