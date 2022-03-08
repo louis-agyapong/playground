a70746122460ee6ccae3b016ec7fa11bdab8d8ae
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-08 10:24:51 +0000

    defined anagram

--- anagram.py
+++ anagram.py
@@ -1,7 +1,16 @@
 """
+Quuestion:
 1. Build a function that takes two paramenters, the initial word and the
-the word you want to check is an anagram of the first word.
+word you want to check is an anagram of the first word.
 2. Output True if an anagram was found and False if not.
+"""
+
+"""
+An anagram is a word or phrase formed by rearranging the letters
+of a different word or phrase, typically using all the original
+letters exactly once. For example, the word anagram itself can be
+rearranged into nag a ram, also the word binary into brainy and the
+word adobe into abode.
 """
 
 
@@ -12,4 +21,4 @@ def check_for_anagram(word1, word2):
     return sorted(word1.lower()) == sorted(word2.lower())
 
 
-print(check_for_anagram("python", "typhona"))
+print(check_for_anagram("adobe", "abode"))
