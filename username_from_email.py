7c811fd0adc7240971b588fcb100df9aeaa16012
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-09 20:48:28 +0000

    return_username function

--- username_from_email.py
+++ username_from_email.py
@@ -1,8 +1,15 @@
-def get_usernames_from_email(emails) -> list:
+def get_usernames_from_email(emails: list) -> list:
     """
     Return usernames from multiple email addresses.
     """
     return [username.split("@")[0] for username in emails]
+
+
+def return_username(email: str) -> str:
+    """
+    Return username from email address.
+    """
+    return email.split("@")[0]
 
 
 if __name__ == "__main__":
@@ -16,3 +23,6 @@ if __name__ == "__main__":
     ]
     usernames = get_usernames_from_email(emails)
     print(usernames)
+
+    x = input("Enter email: ")
+    print(return_username(x))
