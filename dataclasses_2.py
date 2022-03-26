af9f63d6aa084e22b7c0cf43164c632b73414123
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-26 08:14:50 +0000

    Dataclasses

--- dataclasses_2.py
+++ dataclasses_2.py
@@ -1,23 +1,27 @@
 import random
 import string
+from dataclasses import dataclass, field
 
 
 def generate_id() -> str:
     return "".join(random.choices(string.ascii_uppercase, k=12))
 
 
+@dataclass(kw_only=True, slots=True)
 class Person:
-    def __init__(self, name: str, address: str):
-        self.name = name
-        self.address = address
-        self.id = generate_id()
+    name: str
+    address: str
+    id: str = field(init=False, default_factory=generate_id)
+    email_addresses: list = field(default_factory=list)
+    active: bool = True
+    _search_string: str = field(init=False, repr=False)
 
-    def __str__(self):
-        return f"{self.name} ({self.address})"
+    def __post_init__(self) -> None:
+        self._search_string = f"{self.name} {self.address}"
 
 
 def main() -> None:
-    person = Person("Zion", "13 GT Anguah Ln.")
+    person = Person(name="Zion", address="13 GT Anguah Ln.")
     print(person)
 
 
