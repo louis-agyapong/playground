b92154dd50c08745256252230044935d188aa9dd
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-06 08:13:22 +0000

    inheritance

--- inherit_polymorph.py
+++ inherit_polymorph.py
@@ -2,7 +2,7 @@ from black import main
 
 
 class Dog:
-    def __init__(self, name: str, age: int, friendliness: bool) -> None:
+    def __init__(self, name: str, age: int, friendliness: int) -> None:
         self.name = name
         self.age = age
         self.friendliness = friendliness
@@ -12,7 +12,7 @@ class Dog:
 
 
 # class Samoyed(Dog):
-#     def __init__(self, name: str, age: int, friendliness: bool,
+#     def __init__(self, name: str, age: int, friendliness: int,
 #                  fur_color: str) -> None:
 #         super().__init__(name, age, friendliness)
 #         self.fur_color = fur_color
@@ -22,10 +22,40 @@ class Dog:
 
 
 class Samoyed(Dog):
-    def __init__(self, name: str, age: int, friendliness: bool) -> None:
+    def __init__(self, name: str, age: int, friendliness: int) -> None:
+        super().__init__(name, age, friendliness)
+
+
+class Poodle(Dog):
+    def __init__(self, name: str, age: int, friendliness: int) -> None:
+        super().__init__(name, age, friendliness)
+
+    def shedding_amount(self) -> int:
+        return 0
+
+
+class GoldenRetriver(Dog):
+    def __init__(self, name: str, age: int, friendliness: int) -> None:
+        super().__init__(name, age, friendliness)
+
+    def fetch_ability(self) -> int:
+        if self.age < 2:
+            return 8
+        elif self.age < 10:
+            return 10
+        else:
+            return 7
+
+
+class GoldenDoodle(Poodle, GoldenRetriver):
+    def __init__(self, name: str, age: int, friendliness: int) -> None:
         super().__init__(name, age, friendliness)
 
 
 if __name__ == "__main__":
-    sam = Samoyed("Sam", 2, True)
+    sam = Samoyed("Sam", 2, 5)
     print(sam.name, sam.age, sam.friendliness, sam.likes_walks())
+
+    goldie = GoldenDoodle("Goldie", 1, 10)
+    print(f"Fetch Ability: {goldie.fetch_ability()}")
+    print(f"Shedding Amount: {goldie.shedding_amount()}")
