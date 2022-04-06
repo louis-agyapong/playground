41db07e03351b33e84cac1c1db00263173efd096
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-06 10:08:12 +0000

    polymorphism

--- inherit_polymorph.py
+++ inherit_polymorph.py
@@ -1,14 +1,14 @@
-from black import main
-
-
 class Dog:
     def __init__(self, name: str, age: int, friendliness: int) -> None:
         self.name = name
         self.age = age
         self.friendliness = friendliness
 
-    def likes_walks(self) -> bool:
-        return True
+    def likes_walks(self) -> str:
+        return "woof!"
+
+    def barks(self) -> str:
+        return "Awooooo!"
 
 
 # class Samoyed(Dog):
@@ -24,6 +24,9 @@ class Dog:
 class Samoyed(Dog):
     def __init__(self, name: str, age: int, friendliness: int) -> None:
         super().__init__(name, age, friendliness)
+
+    def barks(self) -> str:
+        return "Arf arf!"
 
 
 class Poodle(Dog):
@@ -51,11 +54,17 @@ class GoldenDoodle(Poodle, GoldenRetriver):
     def __init__(self, name: str, age: int, friendliness: int) -> None:
         super().__init__(name, age, friendliness)
 
+    def barks(self) -> str:
+        return "Aroooo!"
+
 
 if __name__ == "__main__":
     sam = Samoyed("Sam", 2, 5)
-    print(sam.name, sam.age, sam.friendliness, sam.likes_walks())
-
+    generic_doggo = Dog("Gene", 1, 10)
     goldie = GoldenDoodle("Goldie", 1, 10)
+
+    print(f"Barks: {sam.barks()}")
     print(f"Fetch Ability: {goldie.fetch_ability()}")
+    print(f"{goldie.__class__.__name__} barks: {goldie.barks()}")
     print(f"Shedding Amount: {goldie.shedding_amount()}")
+    print(f"{generic_doggo.__class__.__name__} barks: {generic_doggo.barks()}")
