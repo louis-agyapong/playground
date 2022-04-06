0c0a0699357fe6559e136a0835124d8b15431427
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-06 07:59:13 +0000

    inheritance

---
+++ inherit_polymorph.py
@@ -0,0 +1,31 @@
+from black import main
+
+
+class Dog:
+    def __init__(self, name: str, age: int, friendliness: bool) -> None:
+        self.name = name
+        self.age = age
+        self.friendliness = friendliness
+
+    def likes_walks(self) -> bool:
+        return True
+
+
+# class Samoyed(Dog):
+#     def __init__(self, name: str, age: int, friendliness: bool,
+#                  fur_color: str) -> None:
+#         super().__init__(name, age, friendliness)
+#         self.fur_color = fur_color
+
+#     def likes_walks(self) -> bool:
+#         return False
+
+
+class Samoyed(Dog):
+    def __init__(self, name: str, age: int, friendliness: bool) -> None:
+        super().__init__(name, age, friendliness)
+
+
+if __name__ == "__main__":
+    sam = Samoyed("Sam", 2, True)
+    print(sam.likes_walks())
